3b5025018c23c7586c41dfe991868cb44e5100a5
Michael Raber <michael.raber@gmail.com>
2014-03-30 07:45:42 -0400

    starting point for a filter view cell

---
+++ FunnlMail/tablecells/FilterViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FilterViewCell.h
+//  FunnlMail
+//
+//  Created by Michael Raber on 3/30/14.
+//  Copyright (c) 2014 FunnlMail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterViewCell : UITableViewCell
+
+@end
---
+++ FunnlMail/tablecells/FilterViewCell.m
@@ -0,0 +1,31 @@
+//
+//  FilterViewCell.m
+//  FunnlMail
+//
+//  Created by Michael Raber on 3/30/14.
+//  Copyright (c) 2014 FunnlMail. All rights reserved.
+//
+
+#import "FilterViewCell.h"
+
+@implementation FilterViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+      
+      self.backgroundColor = [UIColor purpleColor];
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
