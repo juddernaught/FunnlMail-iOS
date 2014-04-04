2a201c606bca18fc1ff78d0354de148036933375
Daniel Judd <juddernaught@gmail.com>
2014-04-04 19:31:15 -0400

    code for message view

---
+++ FunnlMail/viewcontrollers/MsgViewController.h
@@ -0,0 +1,13 @@
+//
+//  MsgViewController.h
+//  FunnlMail
+//
+//  Created by Daniel Judd on 4/4/14.
+//  Copyright (c) 2014 FunnlMail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MsgViewController : ViewController
+
+@end
