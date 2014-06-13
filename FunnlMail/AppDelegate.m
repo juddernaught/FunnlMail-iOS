d4ded287dec2e479d92c00fa9cb5462d4278f86a
Daniel Judd <juddernaught@gmail.com>
2014-06-12 20:23:13 -0400

    FMIPHONE-23 #comment more test changes

--- FunnlMail/AppDelegate.m
+++ FunnlMail/AppDelegate.m
@@ -58,6 +58,7 @@
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
+    
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
