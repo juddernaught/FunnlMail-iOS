f307c62b43fd1561b5cd43b0785b19b07f323fb7
Pranav Herur <PranavHerur@Pranav-Herurs-MacBook-Pro.local>
2014-06-21 20:30:07 -0500

    ph

---
+++ FunnlMail/FunnlMail/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FunnlMail
+//
+//  Created by Daniel Judd on 3/26/14.
+//  Copyright (c) 2014 FunnlMail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MenuViewController.h"
+#import "MMDrawerController.h"
+@class MenuViewController;
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) MenuViewController *menuController;
+@property (strong, nonatomic) MMDrawerController * drawerController;
+@property (strong, nonatomic) UIActivityIndicatorView *appActivityIndicator;
+@end
