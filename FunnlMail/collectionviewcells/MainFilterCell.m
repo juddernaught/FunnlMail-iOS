8f0876077026630e891689153724d7d27d6b78d1
Krunal <krunal.chaudhari@iauro.com>
2014-07-16 13:58:06 +0530

    New icons updated

--- FunnlMail/collectionviewcells/MainFilterCell.m
+++ FunnlMail/collectionviewcells/MainFilterCell.m
@@ -207,7 +207,7 @@
   }else{
         [mailImageView mas_makeConstraints:^(MASConstraintMaker *make) {
           make.top.equalTo(filterTitleLabel.mas_bottom).with.offset(0);
-          make.left.equalTo(self.mas_centerX).with.offset(-(30/2));
+          make.left.equalTo(self.mas_centerX).with.offset(-(45/2));
         }];
     settingsButton.hidden = NO;
     notificationButton.hidden = NO;
