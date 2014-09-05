3d9f1ed86abaa00ce82554ea1fd0e4e8a9b87001
Krunal <krunal.chaudhari@iauro.com>
2014-09-05 23:30:28 +0530

    Minor changes related to animation

--- FunnlMail/viewcontrollers/MainVC.m
+++ FunnlMail/viewcontrollers/MainVC.m
@@ -239,7 +239,7 @@ static NSString *MAIN_FILTER_CELL = @"MainFilterCell";
 //    mainView.alpha = 0;
 //    [UIView commitAnimations];
     [UIView animateWithDuration:ANIMATION_DURATION
-                          delay:0.0
+                          delay:2.0
                         options: UIViewAnimationOptionCurveEaseInOut
                      animations:^{
                          mainView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 2, 2);
