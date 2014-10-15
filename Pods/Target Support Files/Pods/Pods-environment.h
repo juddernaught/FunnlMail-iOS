386e7ab822f57a600b1fe1ed35e196b495e390ca
iKrunal <krunal.chaudhari@iauro.com>
2014-10-15 13:14:24 +0530

    Added new mixpanel 2.5.4 SDK

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -10,7 +10,7 @@
 #define COCOAPODS_POD_AVAILABLE_Mixpanel
 #define COCOAPODS_VERSION_MAJOR_Mixpanel 2
 #define COCOAPODS_VERSION_MINOR_Mixpanel 5
-#define COCOAPODS_VERSION_PATCH_Mixpanel 3
+#define COCOAPODS_VERSION_PATCH_Mixpanel 4
 
 // SSKeychain
 #define COCOAPODS_POD_AVAILABLE_SSKeychain
